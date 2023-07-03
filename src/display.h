2b28ba5b567ea4953debab7809366cb4fd920c2c
Daniel Lundborg <daniel.lundborg@sandvik.com>
2023-07-03 13:29:08 +0200

    move display code to new files display.h and display.cpp
    Signed-off-by: Daniel Lundborg <daniel.lundborg@sandvik.com>

---
+++ src/display.h
@@ -0,0 +1,24 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+extern SDL_Window *window;
+extern SDL_Renderer *renderer;
+extern uint32_t *color_buffer;
+extern SDL_Texture *color_buffer_texture;
+extern int window_width;
+extern int window_height;
+
+// Function prototypes
+extern bool initialize_window(void);
+extern void draw_grid(int distance, uint32_t color);
+extern void draw_rect(int x, int y, int width, int height, uint32_t color);
+extern void render_color_buffer(void);
+extern void clear_color_buffer(uint32_t color);
+extern void destroy_window(void);
+
+#endif
