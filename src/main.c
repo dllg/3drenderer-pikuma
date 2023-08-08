2fbc2bba3f2a9c1e0106b5e3ed6d07ddd449dab7
Daniel Lundborg <daniel.lundborg@sandvik.com>
2023-08-08 11:38:11 +0200

    invert the y values to account for flipped screen y coordinates
    Signed-off-by: Daniel Lundborg <daniel.lundborg@sandvik.com>

--- src/main.c
+++ src/main.c
@@ -188,6 +188,9 @@ void update(void)
             projected_points[j].x *= (window_width / 2.0);
             projected_points[j].y *= (window_height / 2.0);
 
+            // Invert the y values to account for flipped screen y coordinates
+            projected_points[j].y *= -1;
+
             // Translate projected points to the middle of the screen
             projected_points[j].x += (window_width / 2.0);
             projected_points[j].y += (window_height / 2.0);
